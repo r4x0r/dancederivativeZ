df2c23f6cb6ae7369a704e57b3c1e9e6b97360b9
Edward Loke <edwardlokekc@gmail.com>
2014-08-11 17:43:45 +0800

    Add Sendgrid configurations into environement.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+#Using sendgrid to send out the mail. This is the SMTP server
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
