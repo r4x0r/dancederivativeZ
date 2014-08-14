ae08bb9c39412ef165f834fc5df9d2a04fce65e6
Edward Loke <edwardlokekc@gmail.com>
2014-08-14 17:05:32 +0800

    Playing with Fullpagejs
    trial trial

--- app/assets/javascripts/agency.js
+++ app/assets/javascripts/agency.js
@@ -13,7 +13,47 @@ $(function() {
     //     }, 1500, 'easeInOutExpo');
     //     event.preventDefault();
     // });
+
+    $('#fullpage').fullpage({
+        verticalCentered: true,
+        resize : true,
+        // sectionsColor : ['#ccc', '#fff'],
+        anchors:['home', 'whoweare', 'whatwedo', 'instructor', 'events' , 'contact'],
+        scrollingSpeed: 1000,
+        easing: 'easeInOutExpo',
+        menu: '#menu',
+        navigation: false,
+        // navigationPosition: 'right',
+        // navigationTooltips: ['firstSlide', 'secondSlide'],
+        // slidesNavigation: true,
+        // slidesNavPosition: 'bottom',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        autoScrolling: true,
+        scrollOverflow: false,
+        css3: false,
+        paddingTop: '1em',
+        paddingBottom: 0,
+        normalScrollElements: '#element1, .element2',
+        normalScrollElementTouchThreshold: 5,
+        keyboardScrolling: true,
+        touchSensitivity: 15,
+        continuousVertical: false,
+        animateAnchor: true,
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        // //events
+        // onLeave: function(index, nextIndex, direction){},
+        // afterLoad: function(anchorLink, index){},
+        // afterRender: function(){},
+        // afterResize: function(){},
+        // afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        // onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
 });
+
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
