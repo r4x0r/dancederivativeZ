cd3a6f63b6e8e915de90b80e5ea7b9f6ee6aff5c
Edward Loke <edwardlokekc@gmail.com>
2014-08-28 16:04:16 +0800

    Edits to events and partials

--- app/models/events.rb
+++ app/models/events.rb
@@ -3,6 +3,7 @@ class Events < ActiveRecord::Base
   validates :name, :date, presence: true
 
   scope :past, -> { where("date < ?", Time.now) }
+  scope :upcoming_events, -> { where("date > ?", Time.now) }
 
   def is_over?
     if self.date < Time.now
